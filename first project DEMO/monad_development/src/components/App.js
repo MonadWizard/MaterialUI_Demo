75daabf40c83aa0ad4d085a572e165eb984ba364
MonadWizard <monad.wizard.r@gmail.com>
2021-06-16 20:43:21 +0600

    01.complete nav

--- first project DEMO/monad_development/src/components/App.js
+++ first project DEMO/monad_development/src/components/App.js
@@ -1,5 +1,6 @@
 import Container from '@material-ui/core/Container';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './ui/Header';
 import theme from './ui/Theme';
 
@@ -7,7 +8,27 @@ function App() {
     return (
         <div className="App">
             <ThemeProvider theme={theme}>
-                <Header />
+                <BrowserRouter>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={() => <div>Home</div>} />
+                        <Route exact path="/services" component={() => <div>Services</div>} />
+
+                        <Route exact path="/revolution" component={() => <div>revolution</div>} />
+                        <Route exact path="/about" component={() => <div>about</div>} />
+                        <Route exact path="/contact" component={() => <div>Contract</div>} />
+                        <Route exact path="/estimate" component={() => <div>estimate</div>} />
+
+                        <Route
+                            exact
+                            path="/customsoftware"
+                            component={() => <div>Custom Software</div>}
+                        />
+                        <Route exact path="/mobileapps" component={() => <div>mobile apps</div>} />
+                        <Route exact path="/websites" component={() => <div>Web sites</div>} />
+                    </Switch>
+                </BrowserRouter>
+
                 <Container>
                     <h1>Monad Wizard</h1>
                 </Container>
--- first project DEMO/monad_development/src/components/ui/Header.js
+++ first project DEMO/monad_development/src/components/ui/Header.js
@@ -1,10 +1,12 @@
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
@@ -28,33 +30,106 @@ const useStyles = makeStyles((theme) => ({
     logo: {
         height: '3em',
     },
+
+    logoContainer: {
+        padding: 0,
+        '&:hover': {
+            backgroundColor: 'transparent',
+        },
+    },
+
     tabContainer: {
         marginLeft: 'auto',
     },
     tab: {
         ...theme.typography.tab,
-        fontSize: '1.5rem',
-        minWidth: 10,
-        marginLeft: '5px',
-        marginRight: '5px',
+    },
+    button: {
+        ...theme.typography.button,
     },
 }));
 
 export default function Header() {
     const classes = useStyles();
+    const [value, setValue] = useState(0);
+
+    // eslint-disable-next-line no-shadow
+    const handleChange = (e, value) => {
+        setValue(value);
+    };
+
+    useEffect(() => {
+        if (window.location.pathname === '/' && value !== 0) {
+            setValue(0);
+        } else if (window.location.pathname === '/services' && value !== 1) {
+            setValue(1);
+        } else if (window.location.pathname === '/revolution' && value !== 2) {
+            setValue(2);
+        } else if (window.location.pathname === '/about' && value !== 3) {
+            setValue(3);
+        } else if (window.location.pathname === '/contact' && value !== 4) {
+            setValue(4);
+        } else if (window.location.pathname === '/estimate' && value !== 5) {
+            setValue(5);
+        }
+    }, [value]);
+
     return (
         <>
             <ElevationScroll>
                 <AppBar position="fixed">
                     <Toolbar disableGutters>
-                        <img src={logo} alt="company Logo" className={classes.logo} />
-                        <Tabs className={classes.tabContainer}>
-                            <Tab className={classes.tab} label="Home" />
-                            <Tab className={classes.tab} label="Services" />
-                            <Tab className={classes.tab} label="The Revolution" />
-                            <Tab className={classes.tab} label="About us" />
-                            <Tab className={classes.tab} label="Contract us" />
+                        <Button
+                            className={classes.logoContainer}
+                            component={Link}
+                            to="/"
+                            disableRipple
+                            onClick={() => setValue(0)}
+                        >
+                            <img src={logo} alt="company Logo" className={classes.logo} />
+                        </Button>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            className={classes.tabContainer}
+                            indicatorColor="primary"
+                        >
+                            <Tab className={classes.tab} component={Link} to="/" label="Home" />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/services"
+                                label="Services"
+                            />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/revolution"
+                                label="The Revolution"
+                            />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/about"
+                                label="About us"
+                            />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/contact"
+                                label="Contract us"
+                            />
                         </Tabs>
+
+                        <Button
+                            className={classes.button}
+                            component={Link}
+                            to="estimate"
+                            variant="contained"
+                            color="secondary"
+                        >
+                            Estimate
+                        </Button>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
